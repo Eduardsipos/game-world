632368929c40107d167824d81fa3c065cd6f259b
Eduardsipos <104792804+Eduardsipos@users.noreply.github.com>
2023-01-17 17:19:51 +0200

    update

--- src/robots.js
+++ src/robots.js
@@ -4,28 +4,62 @@
 
 
 export const robots = [
-    {
-      id: 1,
-      name: 'Breakout',
-      username: 'Breakout',
-      description: 'des',
-      url : 'https://eduardsipos.github.io/breakout-Game.github.io/'
-      
-    },
-    {
-      id: 2,
-      name: 'Memory-game',
-      username: 'Memory-game',
-      description: 'des',
-      url : 'https://eduardsipos.github.io/memory-game/'
-    },
-    {
-      id: 3,
-      name: 'Space-invaders',
-      username: 'Space-invaders',
-      description: 'des',
-      url : 'https://eduardsipos.github.io/space-invaders/'
-    },
-   
-  ];
-  
+  {
+    id: 1,
+    name: 'Breakout',
+    username: 'Breakout',
+    description: 'Breakout is an arcade video game developed and published by Atari, Inc. and released on May 13, 1976. This is a mini vesion of that, using HTML, CSS and Javascrit',
+    url: 'https://eduardsipos.github.io/breakout-Game.github.io/'
+
+  },
+  {
+    id: 2,
+    name: 'Memory-game',
+    username: 'Memory-game',
+    description: 'Or concentration card game in which all of the cards are laid face down on a surface and two cards are flipped face up over each turn. The object of the game is to turn over pairs of matching cards.',
+    url: 'https://eduardsipos.github.io/memory-game/'
+  },
+  {
+    id: 3,
+    name: 'Space-invaders',
+    username: 'Space-invaders',
+    description: 'Space Invaders[b] is a 1978 shoot-em up arcade game developed by Tomohiro Nishikado. This is also a miniversion of that, using HTML, CSS and Javascrit. ',
+    url: 'https://eduardsipos.github.io/space-invaders/'
+  },
+  {
+    id: 4,
+    name: 'Tic-tac-toe',
+    username: 'Tic-tac-toe',
+    description: 'TBA',
+    url: '#/'
+  },
+
+  {
+    id: 5,
+    name: 'Spacewar!',
+    username: 'Spacewar!',
+    description: 'TBA',
+    url: '#/tba'
+  },
+  {
+    id: 6,
+    name: 'Rock Paper Scissors',
+    username: 'Rock Paper Scissors',
+    description: 'TBA ',
+    url: '#/'
+  },
+  {
+    id: 7,
+    name: 'Whac-a-mole',
+    username: 'Tic-tac-toe',
+    description: 'TBA ',
+    url: '#/'
+  },
+  {
+    id: 8,
+    name: 'Connect Four',
+    username: 'Connect Four',
+    description: 'TBA ',
+    url: '#/'
+  },
+];
